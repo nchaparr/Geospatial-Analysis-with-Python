6070a0b1680ac1ef512e0267bfad6853703b328c
Niamh Chaparro <niamh.chaparro@yhahoo.com>
2016-09-14 13:47:38 -0700

    used pyshp chapter 4

---
+++ use_pyshp.py
@@ -0,0 +1,9 @@
+import shapefile
+shp=shapefile.Reader("point.shp")
+shapes=shp.shapes()
+records=shp.shapeRecords()
+for feature in records:
+    point = feature.shape.points
+    rec=feature.record
+    #print(point[0],point[1],rec)
+    print(point,rec)
